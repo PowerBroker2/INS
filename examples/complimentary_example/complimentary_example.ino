79ed93088d2ad749b11839017d4890bac12f8f09
PB2 <gitstuff2@gmail.com>
2026-01-13 00:18:23 -0500

    Add basic complimentary filter example

--- examples/complimentary_example/complimentary_example.ino
+++ examples/complimentary_example/complimentary_example.ino
@@ -1,57 +1,118 @@
-#include "wmm.h"
-#include "Open_Controller.h"
+#include <Adafruit_LSM6DSOX.h>
+#include <Adafruit_LIS3MDL.h>
+#include "ArduHelpers.h"
+#include "EigenHelpers.h"
+#include "IGRF.h"
+#include "FiltAndCal.h"
 #include "INS.h"
+#include "eigen.h"
+#include <Eigen/Geometry>
 
 
 
 
-using namespace bfs;
+using Eigen::Vector3f;
+using Eigen::Matrix3f;
+using Eigen::Quaternionf;
+using Eigen::AngleAxisf;
 
 
 
 
-sensor_cal     accelCal;
-sensor_cal     gyroCal;
-sensor_cal     magCal;
-OpenController myCtrlr;
-compFilt       ahrs;
-WmmData        earthMagData;
+#define LSM_CS     32
+#define LIS3MDL_CS 31
+
+
+
+
+float lat = 37.7749f;    // San Francisco latitude
+float lon = -122.4194f;  // San Francisco longitude
+
+
+
+
+Adafruit_LSM6DSOX imu;
+Adafruit_LIS3MDL  mag;
+
+sensors_event_t accelEvent;
+sensors_event_t gyroEvent;
+sensors_event_t tempEvent;
+sensors_event_t magEvent;
+
+ProcessingConfigVector accelProcConfig;
+ProcessingConfigVector gyroProcConfig;
+ProcessingConfigVector magProcConfig;
+
+ProcessVector accelProc;
+ProcessVector gyroProc;
+ProcessVector magProc;
+
+CompFilt compAHRS;
+
+Vector3f igrf;
+
+
+
+
+void setupIGRF()
+{
+    igrf << igrf_Bn(lat, lon),
+            igrf_Be(lat, lon),
+            igrf_Bd(lat, lon);
+    
+    compAHRS.updateMag(igrf);
+}
+
+
+
+
+void setupIMU()
+{
+    if (!imu.begin_SPI(LSM_CS, &SPI, 10e6))
+    {
+        Serial.println("Failed to find LSM6DSOX chip");
+        while (1) { delay(10); }
+    }
+
+    imu.setAccelRange(LSM6DS_ACCEL_RANGE_4_G);
+    imu.setGyroRange(LSM6DS_GYRO_RANGE_1000_DPS);
+    imu.setAccelDataRate(LSM6DS_RATE_6_66K_HZ);
+    imu.setGyroDataRate(LSM6DS_RATE_6_66K_HZ);
+
+    accelProc.begin(accelProcConfig);
+    gyroProc.begin(gyroProcConfig);
+}
+
+
+
+
+void setupMag()
+{
+    if (!mag.begin_SPI(LIS3MDL_CS, &SPI, 10e6))
+    {
+        Serial.println("Failed to find LIS3MDL chip");
+        while (1) { delay(10); }
+    }
+
+    mag.setPerformanceMode(LIS3MDL_ULTRAHIGHMODE);
+    mag.setOperationMode(LIS3MDL_CONTINUOUSMODE);
+    mag.setDataRate(LIS3MDL_DATARATE_1000_HZ);
+    mag.setRange(LIS3MDL_RANGE_4_GAUSS);
+
+    magProc.begin(magProcConfig);
+}
 
 
 
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial6.begin(115200);
+    Serial.begin(115200);
 
-  myCtrlr.begin(Serial, false);
-
-  accelCal.lpf_cutoff_hz = 25;
-  myCtrlr.setAccelCal(accelCal);
-
-  gyroCal.intrinsic_bias_vec << -5.71, 3.03, 4.72;
-  gyroCal.lpf_cutoff_hz = 25;
-  myCtrlr.setGyroCal(gyroCal);
-
-//  magCal.intrinsic_cal_mat << 0.02819196,  0.0011868,  -0.00284967,
-//                              0.0011868,   0.03924221,  0.00030842,
-//                             -0.00284967,  0.00030842,  0.03737559;
-  magCal.intrinsic_bias_vec << 4.691612, -12.9932745, 6.313944;
-  magCal.lpf_cutoff_hz = 25;
-  myCtrlr.setMagCal(magCal);
-
-  ahrs.begin(0.10);
-
-  earthMagData = wrldmagm(185,
-                          43.084510,
-                          -75.383543,
-                          2023.5);
-  Vector3d earthMag;
-  earthMag << earthMagData.mag_field_nt[0],
-              earthMagData.mag_field_nt[1],
-              earthMagData.mag_field_nt[2];
-  ahrs.setMag(earthMag);
+    forceAllPinsHigh();
+    setupIGRF();
+    setupIMU();
+    setupMag();
 }
 
 
@@ -59,34 +120,33 @@ void setup()
 
 void loop()
 {
-  myCtrlr.readIMU();
-  
-  Vector3d a = myCtrlr.getCalAccel();
-  Vector3d g = myCtrlr.getCalGyro();
-  Vector3d m = myCtrlr.getCalMag();
-  Vector3d a_in;
-  a_in << 0, 0, 0;
+    unsigned long timestamp = micros();
 
-  ahrs.update(a, g, m, -1, a_in);
+    imu.getEvent(&accelEvent, &gyroEvent, &tempEvent);
+    mag.getEvent(&magEvent);
 
-//  Serial.print("ax:"); Serial.print(a(0));
-//  Serial.print(",ay:"); Serial.print(a(1));
-//  Serial.print(",az:"); Serial.print(a(2));
-//  Serial.println();
+    Vector3f rawAccel;
+    rawAccel << accelEvent.acceleration.x,
+                accelEvent.acceleration.y,
+                accelEvent.acceleration.z;
 
-//  Serial.print("gx:"); Serial.print(g(0));
-//  Serial.print(",gy:"); Serial.print(g(1));
-//  Serial.print(",gz:"); Serial.print(g(2));
-//  Serial.println();
+    Vector3f rawGyro;
+    rawGyro << gyroEvent.gyro.x,
+               gyroEvent.gyro.y,
+               gyroEvent.gyro.z;
 
-//  Serial.print("mx:"); Serial.print(m(0));
-//  Serial.print(",my:"); Serial.print(m(1));
-//  Serial.print(",mz:"); Serial.print(m(2));
-//  Serial.println();
+    Vector3f rawMag;
+    rawMag << magEvent.magnetic.x,
+              magEvent.magnetic.y,
+              magEvent.magnetic.z;
+    
+    Vector3f calAccel = accelProc.process(rawAccel);
+    Vector3f calGyro  = gyroProc.process(rawGyro);
+    Vector3f calMag   = magProc.process(rawMag);
 
-  Serial.print("Pitch:"); Serial.print(ahrs.getPitch());
-  Serial.print(",Roll:"); Serial.print(ahrs.getRoll());
-  Serial.print(",Yaw:");  Serial.println(ahrs.getYaw());
+    compAHRS.update(calAccel, calGyro, calMag, timestamp);
+    Vector3f eulers = compAHRS.get_b_R_ang_n();
 
-  delay(1);
+    printVec(eulers);
+    delay(1);
 }
